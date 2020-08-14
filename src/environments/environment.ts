5340fafe7223424e12fbdce32f1a6dc52547b8a9
Mushahid Khan <mushahid.khan@codeb.online>
2020-08-14 15:27:06 +0530

    environemnt init.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBhryU_qSCgKRX3OTgoVaW5xGyPeo3aS_g",
+    authDomain: "e-commere-product.firebaseapp.com",
+    databaseURL: "https://e-commere-product.firebaseio.com",
+    projectId: "e-commere-product",
+    storageBucket: "e-commere-product.appspot.com",
+    messagingSenderId: "384324550722",
+    appId: "1:384324550722:web:cc13ab7827a04f280a2ddb",
+    measurementId: "G-GT3SZHTDFT"
+  }
 };
 
 /*
